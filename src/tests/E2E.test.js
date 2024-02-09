bad127e2a62fd7ba72ffe64c13a9939a14320270
LeaveThePast <Ramil.Budagli@yandex.ru>
2024-02-09 22:39:57 +0400

    Update

--- src/tests/E2E.test.js
+++ src/tests/E2E.test.js
@@ -1,30 +1,33 @@
-const puppeteer = require('puppeteer');
+import { launch } from "puppeteer";
 
-describe('Card Validator', () => {
+describe("Card Validator", () => {
   let browser;
   let page;
 
   beforeAll(async () => {
-    browser = await puppeteer.launch();
+    browser = await launch();
     page = await browser.newPage();
-    await page.goto('http://127.0.0.1:5500/dist/index.html');
+    await page.goto("https://leavethepast.github.io/JS_Card_Validator/");
   });
 
   afterAll(() => {
     browser.close();
   });
 
-  test('should display validation result when form is submitted', async () => {
-    await page.type('#cardNumber', '4532015112830366');
+  test("should display validation result when form is submitted", async () => {
+    await page.type("#cardNumber", "4532015112830366");
     await page.click('input[type="submit"]');
-    await page.waitForSelector('.validationResult.show');
-    const result = await page.$eval('.validationResultText', el => el.textContent);
-    expect(result).toBe('Номер карты действителен!');
+    await page.waitForSelector(".validationResult.show");
+    const result = await page.$eval(
+      ".validationResultText",
+      (el) => el.textContent
+    );
+    expect(result).toBe("Номер карты действителен!");
   });
 
-  test('should display card type when card number is input', async () => {
-    await page.type('#cardNumber', '4532015112830366');
-    const cardTypeElement = await page.$('.cardTypeSelect');
+  test("should display card type when card number is input", async () => {
+    await page.type("#cardNumber", "4532015112830366");
+    const cardTypeElement = await page.$(".cardTypeSelect");
     expect(cardTypeElement).toBeTruthy();
   });
 });
